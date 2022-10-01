aec7ba3de7cbcce3a5ec98f228d2b911230b93e5
Dnyanesh1045 <kharpude104510@gmail.com>
2022-10-02 00:01:49 +0530

    Developer2 Commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## THIS IS FIRST TITORIAL OF GIT
 ## Try for Best God with us.git 
 ## Everything is ok God have better plan for you.
+## Patience , persistence and perspiration make an unbeatable combination for success.
--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-list1 = list(range(0,50))
-empty_list=[]
-for i in list1:
-    if i%2==0:
-        empty_list+=[i]
-print(empty_list)
+# list1 = list(range(0,50))
+# empty_list=[]
+# for i in list1:
+#     if i%2==0:
+#         empty_list+=[i]
+# print(empty_list)
+
+str1 = "Hello Bapu How are you, You have done lots of thing for us."
+empty_str=""
+for i in str1:
+    if i not in empty_str:
+        empty_str+=i
+print("Duplicate are removed :",empty_str)
